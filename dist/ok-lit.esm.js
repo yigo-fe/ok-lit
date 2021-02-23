748d919a150460e637736ad6535978490987e044
hcg1023 <2293885211@qq.com>
2021-02-23 11:36:47 +0800

    feat: 修复打包新语法问题

--- dist/ok-lit.esm.js
+++ dist/ok-lit.esm.js
@@ -3,6 +3,31 @@ export * from 'lit-html';
 import { stop, isRef, isReactive, effect, shallowReactive } from '@vue/reactivity';
 export * from '@vue/reactivity';
 
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation.
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
+***************************************************************************** */
+
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
 const error = console.error;
 const toString = Object.prototype.toString;
 const getExactType = (arg) => toString.call(arg).slice(8, -1);
@@ -36,7 +61,7 @@ function isJSONString(value) {
             return result;
         }
     }
-    catch {
+    catch (_a) {
     }
     return false;
 }
@@ -86,7 +111,7 @@ function validateProp(key, config, props) {
             return true;
         }
         else {
-            const transform = userTransform ?? isJSONString;
+            const transform = userTransform !== null && userTransform !== void 0 ? userTransform : isJSONString;
             const jsonResult = transform(value);
             if (jsonResult && isType(jsonResult)) {
                 value = jsonResult;
@@ -108,7 +133,7 @@ function validateProp(key, config, props) {
                 const toFunction = (value) => {
                     return new Function(`return ${value}`)();
                 };
-                const transform = userTransform ?? toFunction;
+                const transform = userTransform !== null && userTransform !== void 0 ? userTransform : toFunction;
                 const fn = transform(value);
                 isFunction(fn) && (value = fn);
                 return true;
@@ -141,18 +166,20 @@ function validateProp(key, config, props) {
     props[key] = value;
     callValidator(validator, key, value);
 }
-async function callValidator(validator, key, value) {
-    const validatorFn = validator ?? boolFn;
-    let validateResult;
-    try {
-        validateResult = await validatorFn(value);
-    }
-    catch (err) {
-        error(err.message);
-    }
-    if (!validateResult) {
-        error(`the props.${key} validate error`);
-    }
+function callValidator(validator, key, value) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const validatorFn = validator !== null && validator !== void 0 ? validator : boolFn;
+        let validateResult;
+        try {
+            validateResult = yield validatorFn(value);
+        }
+        catch (err) {
+            error(err.message);
+        }
+        if (!validateResult) {
+            error(`the props.${key} validate error`);
+        }
+    });
 }
 
 /**
